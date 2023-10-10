4d062b47ea6b7e5f149e17a6fd2367bb1fd5525d
Gregório Ponciano <gregorioponci.91@gmail.com>
2023-10-10 13:14:00 -0300

    Initial commit

---
+++ menubar.js
@@ -0,0 +1,10 @@
+function clicarmenu() {
+    if ( itensmenu.style.display == 'block') {
+        itensmenu.style.display = 'none'
+    } else {
+        itensmenu.style.display = 'block'
+    }
+}
+
+
+
