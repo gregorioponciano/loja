a13f8ba978c3d8632f6a0299340f84ad83d11f91
Gregório Ponciano <gregorioponci.91@gmail.com>
2023-10-12 18:45:14 -0300

    s

--- menubar.js
+++ menubar.js
@@ -9,15 +9,34 @@ function toggleSidebar()
     if(showSidebar) 
     {
         navigationHeader.style.marginLeft = '-10vw';
-        navigationHeader.style.animationName = 'showSidebar';
-        content.style.filter = 'blur(2px)';
+        navigationHeader.style.animationName = 'showSidebar';    // animaçao abrir o menu devagar ligado a @keyframe na css
+        content.style.filter = 'blur(2px)';                      // embassa com filtro embassado
     }
     else 
     {
         navigationHeader.style.marginLeft = '-100vw';
-        navigationHeader.style.animationName = '';
+        navigationHeader.style.animationName = '';              // remove animaçao abrir o menu devagar ligado a @keyframe na css
+        content.style.filter = '';                              // remove o filtro embassado
     }
+} 
+
+    //para fechar o menu clicando fora 
+
+function closeSidebar() {
+    if(showSidebar) {
+
+    }
+    toggleSidebar();
 }
+
+     // mudou tamanho da tela tira o filtro da tela grnade
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar) {
+        toggleSidebar();
+    }
+});
+
 
 
 
