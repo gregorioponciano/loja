3e6f61e33ea8fdfb76224472aa8d5cd9a5c3cc7a
Gregório Ponciano <gregorioponci.91@gmail.com>
2023-12-06 04:52:21 -0300

    sim

---
+++ status.js
@@ -0,0 +1,29 @@
+function verificarStatusFuncionamento() {
+    const agora = new Date();
+    const diaSemana = agora.getDay(); // Obtém o dia da semana (0 = Domingo, 1 = Segunda, ..., 6 = Sábado)
+    const hora = agora.getHours();
+
+    // Verifica se é sábado ou domingo
+    if (diaSemana === 0 || diaSemana === 6) { 
+      if (hora >= 7 && hora < 12) {  // De sabado e domingo
+      } else {
+        return 'Fechado';
+      } 
+    } else if (hora >= 18 && hora < 22) { // De segunda a sexta
+      return 'Aberta';
+    } else {
+      return 'Fechado';
+    }
+  }
+
+  const status = verificarStatusFuncionamento();
+  const statusElement = document.getElementById('status');
+  statusElement.textContent = `${status}`;
+
+  if (status === 'Aberta') {
+  statusElement.textContent = 'Aberta';
+  statusElement.style.color = 'green';
+} else {
+  statusElement.textContent = 'Fechado';
+  statusElement.style.color = 'red';
+}
