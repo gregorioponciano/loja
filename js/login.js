8058e262a24a5b88ad9456b030a195c5f26aad75
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-04 00:06:14 -0300

    s

--- js/login.js
+++ js/login.js
@@ -1,10 +1,13 @@
-var nome = document.getElementById('nome').value //value é para pegar oque foi digitado dentro do input 
-var senha = document.getElementById('senha').value
+
 
 function logar() {
+
+  var nome = document.getElementById('nome').value //value é para pegar oque foi digitado dentro do input 
+  var senha = document.getElementById('senha').value
+
 if(nome == "admin" && senha == "admin123") {
     alert(`susseso`);
-    location.href = "inicio.html";  // location.href procura uma pagina html que vai ser redirecionada
+    location.href = "inicio-login.html";  // location.href procura uma pagina html que vai ser redirecionada
 }else{
     alert('Usuario ou senha incorretos');
 }
