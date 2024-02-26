f708fef2c529b2210302875de51aa1d89428e589
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-26 00:04:58 -0300

    s

--- js/termos.js
+++ js/termos.js
@@ -7,7 +7,7 @@ function toggleContent(id) {
         }
     }
     if (content.style.display === 'block') {
-        content.style.display = 'none';
+        content.style.display = 'block';
     } else {
         content.style.display = 'block';
     }
