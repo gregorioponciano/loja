410b8572e32cacabde23f0c816cdcb8286e724b6
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-03-16 21:20:01 -0300

    sim

---
+++ js/cadastro.js
@@ -0,0 +1,38 @@
+const menostermos = document.querySelector('#ler-menos-termos')
+const maistermos = document.querySelector('#ler-mais-termos')
+const conteudo = document.querySelector('#p-ler-mais-termos')
+
+
+function maisTermos() {
+    conteudo.style.display = 'block'
+    maistermos.style.display = 'none'
+    menostermos.style.display = 'block'
+}
+
+
+function menosTermos() {
+    menostermos.style.display = 'none'
+    maistermos.style.display = 'block'
+    conteudo.style.display = 'none'
+}
+
+
+function cadastro() {
+        //cadastro
+    var nome = document.querySelector('#nome').value
+    var sobrenome = document.querySelector('#sobrenome').value
+    var email = document.querySelector('#email').value
+    var senha = document.querySelector('#senha').value
+    var termos = document.querySelector('#concordo').value
+    
+    var cadastro =document.querySelector('.input-cadastro')
+
+    if (nome === "" || sobrenome === "" || email ==="") {
+        alert(`prencha os dados`)
+        return
+    }
+    alert(`obrigado por se cadastra ${nome} ${sobrenome} mais este sistema ainda nao esta Disponivel no momento por favor entre em contato pelo whatszap '014 99176-1256'`)
+    
+
+}
+
