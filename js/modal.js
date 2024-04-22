6b72f911b82a52e3996c11e6c5ecb5bbbf16d119
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-04-21 21:42:45 -0300

    s

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,11 @@
-const openModalButton = document.getElementById('open-modal');
-const modal = document.querySelector('.modal');
-const closeButton = document.querySelector('.close-button');
-
 // Função para abrir o modal
-function openModal() {
-    modal.classList.add('active');
+function abrirModal() {
+    var modal = document.querySelector('.modal');
+    modal.style.display = 'block';
 }
 
 // Função para fechar o modal
-function closeModal() {
-    modal.classList.remove('active');
+function fecharModal() {
+    var modal = document.querySelector('.modal');
+    modal.style.display = 'none';
 }
-
-// Adicionar eventos de click aos botões
-openModalButton.addEventListener('click', openModal);
-closeButton.addEventListener('click', closeModal);
-
-// Fechar o modal ao clicar fora da área de conteúdo
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        closeModal();
-    }
-});
