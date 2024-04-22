56b9cdaa2f9f353466f50fc1d662413cc0001b68
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-04-21 21:50:09 -0300

    s

--- js/modal.js
+++ js/modal.js
@@ -9,3 +9,15 @@ function fecharModal() {
     var modal = document.querySelector('.modal');
     modal.style.display = 'none';
 }
+
+        // abrir o contato de whats
+var botaoAbrirSite = document.getElementById('abrirSite');
+
+        // Adiciona um evento de clique ao botão
+botaoAbrirSite.addEventListener('click', function() {
+    // URL do site que você deseja abrir
+    var url = 'https://wa.me/5514991761256?text=Ol%C3%A1%2C+Td+bem%3F+Como+posso+ajudar%3F';
+
+    // Abre o site em uma nova janela
+    window.open(url);
+});
